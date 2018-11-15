2c174dfbe997be8e5958bafe56374850ea5e219f
Ramkumar Paranjothy <ramkumar_paranjothy@homedepot.com>
2018-11-15 15:05:38 -0600

    added build for easy porting

---
+++ build/precache-manifest.5c290e9d4593e315530995c7d4959b9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c98adb20b36f77820a0c",
+    "url": "/static/js/main.c98adb20.chunk.js"
+  },
+  {
+    "revision": "3a878af09e65cc63ff7e",
+    "url": "/static/js/1.3a878af0.chunk.js"
+  },
+  {
+    "revision": "c98adb20b36f77820a0c",
+    "url": "/static/css/main.ef820e62.chunk.css"
+  },
+  {
+    "revision": "3a878af09e65cc63ff7e",
+    "url": "/static/css/1.c220b55d.chunk.css"
+  },
+  {
+    "revision": "a9bfe55fe8519ea103e4ec28259c74da",
+    "url": "/index.html"
+  }
+];
