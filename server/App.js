79418b2fed6676c75461368b6877731d7edbede8
Ramkumar Paranjothy <ramkumar_paranjothy@homedepot.com>
2018-11-19 16:54:10 -0600

    PCF Deploy

--- server/App.js
+++ server/App.js
@@ -2,14 +2,14 @@ const path = require('path');
 const express = require("express");
 
 const app = express();
-
-app.use(express.static('../build/'));
+const port=process.env.PORT||3000;
+app.use(express.static('./build/'));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
+  res.sendFile(path.join(__dirname, './build/index.html'));
 });
 
 
-app.listen(3000, () => {
-  console.log("Server Started on port 3000");
+app.listen(port, () => {
+  console.log(`Server Started on port ${port}`);
 });
