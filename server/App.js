2107b65d5bb02f52d951c556980e41acdd89d520
Ramkumar Paranjothy <ramkumar_paranjothy@homedepot.com>
2018-11-19 17:15:33 -0600

    build folder path

--- server/App.js
+++ server/App.js
@@ -3,10 +3,10 @@ const express = require("express");
 
 const app = express();
 const port=process.env.PORT||3000;
-app.use(express.static('./build/'));
+app.use(express.static('../build/'));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, './build/index.html'));
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 
