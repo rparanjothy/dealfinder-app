29a8b43fd3c53e619f2e50f72bae644391b12418
Ramkumar Paranjothy <ramkumar_paranjothy@homedepot.com>
2018-11-20 11:53:38 -0600

    Prod App for dealFinder

---
+++ server/APIServer/gcp-deploy/deployAPIServer-GCP.sh
@@ -0,0 +1,20 @@
+sudo su
+alias c=clear
+alias l='ls -latr'
+apt-get install python-pip
+apt-get install git
+mkdir /var/dealfinder
+cd /var/dealfinder
+mkdir APIServer
+mkdir APIServer/data
+cd ./APIServer
+sudo gsutil -m cp -r gs://hd-www-dev-catalog-data/dealfinder/APIServer/* .
+sudo gsutil -m cp gs://hd-www-prod-catalog-data/extracts/onlineprice-output/OnlineWas2PriceInventoryExtract.dat	 ./data/OnlineWas2PriceInventoryExtract.del
+
+sudo pip install virtualenv
+sudo virtualenv -p python venv
+source ./venv/bin/activate
+
+cd /var/dealfinder/APIServer
+sudo pip install -r /var/dealfinder/requirements.txt
+sudo gunicorn savingsAPI:app --bind 0.0.0.0:80
--- src/config/AppConfig.js
+++ src/config/AppConfig.js
@@ -1,11 +1,15 @@
-
-// const APIURL = "http://172.27.156.206:5000"
-// const APIURL = "http://35.227.24.194:80"
  
 // const IMGURL = "http://origin-api.gcp-prod.homedepot.com/catalog/v1/items/"
 
+//localhost
 // const APIURL = "http://localhost:5000"
-const APIURL = "https://dealfinderapi.apps-np.homedepot.com"
+
+//localhost over LAN
+// const APIURL = "http://172.27.156.206:5000"
+
+// GCP API Server
+const APIURL = "http://35.243.143.253"
+// const APIURL = "https://dealfinderapi.apps-np.homedepot.com"
 const IMGURL = "http://origin-api.gcp-stage.homedepot.com/catalog/v1/items/"
 const SEARCHURL="http://www.homedepot.com"
 const config={APIURL,IMGURL,SEARCHURL}
