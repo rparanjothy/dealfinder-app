1441a62d897f22576eb112552aeae681e27d1953
Ramkumar Paranjothy <ramkumar_paranjothy@homedepot.com>
2018-11-20 21:04:43 -0600

    log change build

---
+++ build/precache-manifest.7c33aecb87938cba237df0c0aa8839b1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "302a4ce6276a84d463a1609f6ffeb438",
+    "url": "/static/media/flask.302a4ce6.png"
+  },
+  {
+    "revision": "d85377cd169a1ea91f8b",
+    "url": "/static/css/main.ead42333.chunk.css"
+  },
+  {
+    "revision": "9e3f014e7e5ff99fd59be781c63c9cb5",
+    "url": "/static/media/gunicorn.9e3f014e.png"
+  },
+  {
+    "revision": "94427b0697fbebae5d48",
+    "url": "/static/js/1.94427b06.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ca2ceddcd9c28a22c449286f3fe66751",
+    "url": "/static/media/python.ca2ceddc.png"
+  },
+  {
+    "revision": "d85377cd169a1ea91f8b",
+    "url": "/static/js/main.d85377cd.chunk.js"
+  },
+  {
+    "revision": "f8dab57d048fabd69ea16c67e1615b86",
+    "url": "/static/media/node.f8dab57d.png"
+  },
+  {
+    "revision": "ada36a9d6b53368db9501d6864a319df",
+    "url": "/static/media/pandas.ada36a9d.png"
+  },
+  {
+    "revision": "692adb35708d9279ca7eb56c71bfc102",
+    "url": "/static/media/react.692adb35.png"
+  },
+  {
+    "revision": "94427b0697fbebae5d48",
+    "url": "/static/css/1.b5581ba8.chunk.css"
+  },
+  {
+    "revision": "b7fd2840d115d1ba748eb08f06be7f30",
+    "url": "/index.html"
+  }
+];
